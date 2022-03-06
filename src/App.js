73808a541926172e998ba50b435f8b6a10dc52d6
HosseinGhiasi <hosseinghiasi5727@gmail.com>
2022-03-06 14:27:29 +0330

    added files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="w-full h-[100vh] text-white text-center bg-gradient-to-r from-purple-400 to-blue-600">
       <h1 className="text-4xl pt-5 ">خوش آمدید</h1>
-      <a href={resume} download className='bg-yellow-500 text-center inline-block mt-10 w-[350px] px-5 py-2 rounded-sm  text-[19px] animate-bounce '>برای دریافت رزومه حسین غیاثی کلیک کنید</a>
+      <a href={resume} download className='bg-yellow-500 text-center inline-block mt-10 w-[360px] px-5 py-2 rounded-sm  text-[19px] animate-bounce '>برای دریافت رزومه حسین غیاثی کلیک کنید</a>
     </div>
   );
 }
